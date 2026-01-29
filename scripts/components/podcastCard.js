340474a2d5b6e14809f9f915c62defb9b859c6b0
Emmanuel <07942jerry@gmail.com>
2026-01-29 22:13:57 +0200

    changing from class to functon

---
+++ scripts/components/podcastCard.js
@@ -0,0 +1,34 @@
+// @ts-check
+import { createModal} from "./modal.js";
+
+    /**
+     * Creates a podcast card element.
+     * @param {Object} podcast - The podcast data.
+     * @param {string} podcast.id - The unique identifier of the podcast.
+     * @param {string} podcast.image - The URL of the podcast image.
+     * @param {string} podcast.title - The title of the podcast.
+     * @param {number[]} podcast.seasons - The number of seasons on the podcast.
+     * @param {number[]} podcast.genres - The genres of the podcast.
+     * @param {string} podcast.updated - The last updated date of the podcast.
+     * @returns {HTMLElement} The podcast card element.
+     */
+
+
+export function createPodcastCard(podcast) {
+    const podcastList = document.getElementById('podcast-cards');
+    const card = document.createElement('div');
+    card.className = 'podcast-card';
+    card.innerHTML = `
+        <img src="${podcast.image}" alt="Podcast Image" class="podcast-image">
+        <div class="podcast-info">
+            <h3 class="podcast-title">${podcast.title}</h3>
+            <p class="podcast-seasons">📁 ${podcast.seasons} seasons</p>
+            <p class="podcast-genres">${podcast.genres}</p>
+            <p class="podcast-updated">Last Updated: ${podcast.updated}</p>
+        </div>
+    `;
+    card.addEventListener('click', () => {
+        createModal(podcast);
+    });
+    return card;
+}
