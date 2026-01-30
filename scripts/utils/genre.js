e29e994727635f2005c7ee48af829cb74b5b54b6
Emmanuel <07942jerry@gmail.com>
2026-01-30 15:28:03 +0200

    creating function for genre

---
+++ scripts/utils/genre.js
@@ -0,0 +1,11 @@
+import { genres } from '../data.js';
+
+
+export const genre = {
+getGenreNames(genreIds) {
+  return genreIds.map(id => {
+    return genres.find(genre => genre.id === id)?.title|| 'Unknown';
+  });
+}
+};
+
