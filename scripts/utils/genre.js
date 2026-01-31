da76f4140c2534cc5db26571b0865971ddd588fb
Emmanuel <07942jerry@gmail.com>
2026-01-31 11:32:02 +0200

    creating function to map genres

--- scripts/utils/genre.js
+++ scripts/utils/genre.js
@@ -2,10 +2,10 @@ import { genres } from '../data.js';
 
 
 export const genre = {
-getGenreNames(genreIds) {
-  return genreIds.map(id => {
-    return genres.find(genre => genre.id === id)?.title|| 'Unknown';
-  });
-}
+  getGenreNames(genreIds) {
+    return genreIds.map(id => {
+      return genres.find(genre => genre.id === id)?.title|| 'Unknown';
+    });
+  }
 };
 
