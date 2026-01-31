850c92dae471e45dcce7b25991e188073e83914e
Emmanuel <07942jerry@gmail.com>
2026-01-31 08:25:03 +0200

    creating function to format dates

---
+++ scripts/utils/dateConverter.js
@@ -0,0 +1,10 @@
+
+
+// Function to render podcast cards
+export const dates = {
+    formatDate(dateString) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString(undefined, options);
+    }
+};
